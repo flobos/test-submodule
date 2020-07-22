dc058ec18fffe17d542e9939778e1ac6d5844933
Fernando <fernandolobos@gmail.com>
2020-07-21 20:56:13 -0400

    init

---
+++ src/main/java/com/moss/latam/testsubmodule/TestSubmoduleApplication.java
@@ -0,0 +1,13 @@
+package com.moss.latam.testsubmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSubmoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSubmoduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moss/latam/testsubmodule/TestSubmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moss.latam.testsubmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSubmoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
